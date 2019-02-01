95ba0df58efd33bb046e78e97c2ebb04d6deb41e
Soojin Choi <schoi4@stuy.edu>
2019-02-01 00:05:06 -0500

    k01

--- 01_canvas/clickdraw.js
+++ 01_canvas/clickdraw.js
@@ -1,71 +1,80 @@
-// Soojin Choi
-// SoftDev2 pd08
-// K00 -- I See a Red Door...
-// 2019-01-30
+// OnePlusOneIsThree -- Soojin Choi, Tabassum Fabiha, Isaac Jon
+// SoftDev2 pd7
+// K01 - ...and I want to Paint It Better
+// 2019-01-31
 
-var c = document.getElementById('slate');
-//console.log(c);
-var ctx = c.getContext("2d");
+const c = document.getElementById("slate");
+const ctx = c.getContext("2d");
 
-var clearCanvas = function(){
-  //var c = document.getElementById('slate');
-  //console.log(c);
-  //var ctx = c.getContext("2d");
-  ctx.clearRect(0,0,600,600);
-};
+var drawState = "BOX" //"BOX" is box, "DOT" is dot
 
-var drawRect = function(e){
+var toggleState = function(e) { //Toggles between placing dot or a box
 
-  // console.log(e);
-  var corX = e.offsetX - 25;
-  var corY = e.offsetY - 25;
-
-  ctx.fillRect(corX, corY, 50, 50);
-
-};
-
-var drawCircle = function(e){
-
-  //console.log(e);
-  var corX = e["layerX"] + 25;
-  var corY = e["layerY"] + 25;
-
-  ctx.beginPath()
-  ctx.ellipse(corX, corY, 25, 25, Math.PI / 4, 0, Math.PI);
-  ctx.stroke();
+    if(drawState == "BOX")
+    {
+	drawState = "DOT";
+	console.log(drawState);
+	msg.innerHTML = "Current State: DOT";
+    }
+    else
+    {
+	drawState = "BOX";
+	console.log(drawState);
+	msg.innerHTML = "Current State: BOX";
+    };
 
 };
 
-var draw = function(e){
+var getMousePos = function(evt) { //Same as using evt.offsetX and evt.offsetY
 
-  var type = document.getElementById('type');
-  if (type.innerHTML == " squares! "){
-    drawRect(e);
-  }
-  else{
-    drawCircle(e);
-  }
+    // e.offsetX is used to return the x coordinate of mouse cursor
+    // in relation to the div in which your cursor is in
 
-};
+    //e.offsetY same as above, but returns y coordinate instead
 
-var toggle = function(){
+    return {
+	x: evt.offsetX,
+	y: evt.offsetY
+    };
+}
 
-  var type = document.getElementById('type');
-  console.log(type.innerHTML)
-  if (type.innerHTML == " squares! "){
-    type.innerHTML = " dots! ";
-    //curr = drawCircle;
-  }
-  else{
-    type.innerHTML = " squares! ";
-    //curr = drawRect;
-  }
-};
+var drawShape = function(evt) { //Draws the shape based on
 
-var button = document.getElementById("clear");
-button.addEventListener('click', clearCanvas);
+    if (drawState == "BOX")
+    {
+	ctx.fillStyle = '#0000ff'; // change color of fill
+	//ctx.fillRect(pos.x, pos.y, 20, 20);
+	ctx.fillRect(evt.offsetX, evt.offsetY, 20, 20) // display rectangle
+    }
 
-var tbutton = document.getElementById("toggle");
-tbutton.addEventListener('click', toggle);
+    if (drawState == "DOT")
+    {
+	// ctx.beginPath() is used before starting a new path,
+	// it can be used to draw lines, ellipses, arcs, or rectangles
+	ctx.beginPath();
 
-c.addEventListener('click', draw);
+	// create a dot on ctx
+	//ctx.ellipse(pos.x, pos.y, 12, 12, Math.PI / 4, 0, 2 * Math.PI);
+	ctx.ellipse(evt.offsetX, evt.offsetY, 12, 12, Math.PI / 4, 0, 2 * Math.PI);
+
+	//ctx.stroke();
+
+	ctx.fillStyle = '#0000ff'; // change color of fill
+	ctx.fill() // display shape on page
+    }
+}
+
+var clear = function(e) { // clears the canvas
+
+    ctx.clearRect(0, 0, c.width, c.height);
+}
+
+var t = document.getElementById("toggle");
+t.addEventListener('click', toggleState)
+
+var cls = document.getElementById("clear");
+cls.addEventListener('click', clear);
+
+// Not incorporated in the program:
+// e.preventDefault()
+// --- when called it prevents the default event from occurring
