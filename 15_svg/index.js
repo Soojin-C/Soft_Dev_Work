53a97e39a8757a88550f2a57cd266ff49efa07ff
Soojin Choi <schoi4@stuy.edu>
2019-03-25 02:29:40 -0400

    k15

--- 15_svg/index.js
+++ 15_svg/index.js
@@ -1,4 +1,7 @@
-var scatter_data = [[0,0],[2,3],[4,5],[6,7],[7,8]]
+var scatter_data = [[0.3,1302],[0.3,1510],[0.31,1641],[0.31,1555],[0.32,1468],[0.32,1202]]
+//Data about the weight and price of diamonds
+//https://www2.stetson.edu/~jrasp/data.htm
+
 //d3.tsv("data.tsv", type, function(error, data) {
 //  x.domain([0, d3.max(data, function(d) { return d.value; })]);
 
@@ -6,16 +9,16 @@ var svg = d3.select("body")
             .append("svg")
             .attr("width", 500)
             .attr("height", 500);
-
+var space = 0;
 var dots = svg.selectAll("dots")
               .data(scatter_data)
               .enter()
               .append("circle")
               .attr("cx", function(d) {
-                return (d[0] * 50) + 5
+                return 30 + ((d[0] - 0.3) * 9000 )
               })
               .attr("cy", function(d){
-                return (500 - (d[1] * 50)) - 5
+                return 500 - ((d[1] -1290))
               })
               .attr("r", 5)
               .attr("fill", "black");
@@ -25,10 +28,10 @@ var label = svg.selectAll("label")
               .enter()
               .append("text")
               .attr("x", function(d) {
-                return (d[0] * 50) + 1
+                return 30 + ((d[0] - 0.3) * 9000 )
               })
               .attr("y", function(d){
-                return (500 - (d[1] * 50)) - 11
+                return 500 - ((d[1] -1290)) - 10
               })
               .attr("fill", "blue")
               .text(function(d){
