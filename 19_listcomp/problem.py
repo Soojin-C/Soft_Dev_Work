239b00283a5162f1121050e32329e1c9111a7fe1
Soojin Choi <schoi4@stuy.edu>
2019-04-16 22:25:46 -0400

    k19

--- 19_listcomp/problem.py
+++ 19_listcomp/problem.py
@@ -1,16 +1,17 @@
 # Team Teal
 # Angela Tom and Soojin Choi
-#
-# 
+# K19 -- Ready, Set, Math
+# 2019-04-16
 
 # Union
 # 
 def union(set1, set2):
-    pass
-    #return [x for x in set1 if x not in set2 else y for y in set2]
+    return [x for x in set1 if x not in set2] + set2    
 
-#print("Union")
+print("Union")
 print(union([1,2,3],[2,3,4]))
+print(union([],[]))
+print(union([3,6,8],[1,5,6]))
 
 # Intersection
 # {1,2,3} and {2,3,4} -> {2,3}
@@ -25,7 +26,7 @@ print(inter([1,2,3],[2,3,4]))
 def complement(set1, set2):
     return [x for x in set1 if x not in set2]
 
-print("complement")
+print("Complement")
 print(complement([1,2,3],[2,3,4]))
 
 #symmetric difference
@@ -34,7 +35,10 @@ def sym(set1, set2):
     list = [x for x in set1 if x not in set2]
     list2 = [y for y in set2 if y not in set1]
     return list + list2
-    
+
+print("Symmetric Difference")
+print(sym([1,2,3],[2,3,4]))
+
 # cartesian product of A and B
 # {1,2} {red, white}
 # {(1, red)(1, white)(2, red)(2, white)}
@@ -42,5 +46,18 @@ def sym(set1, set2):
 def cart(set1, set2):
     return [(x,y) for x in set1 for y in set2]
 
-print("cart")
+print("Cartesian Product")
 print(cart([1,2],["red", "white"]))
+
+
+
+
+
+def sortforset(set):
+    pass
+
+def triple(num):
+    return [(a,b,c) for a in range(num) for b in range(a) for c in range(b) if a*a == b*b + c*c]
+
+print(triple(10))
+    
